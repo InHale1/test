c97c57e22ead00b6503d79fd1214baad1a83a315
InHale1 <Luciferus-666@mail.ru>
2014-04-05 14:01:15 +0300

    Revert "first commit"
    This reverts commit ab2e38bcbdc180524057d2e1eaf06a3d521551d2.

--- config.php
+++
@@ -1,8 +0,0 @@
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-<?php
-	$host = '127.4.193.2';
-	$usr = 'adminwkVHDGk';
-	$pwd = '5_ntb-C-9l2r';
-	$nameconnect = mysql_connect($host, $usr, $pwd) ;
-	$res = mysql_select_db ('BankClient', $nameconnect) || die(mysql_error());  
-?>
