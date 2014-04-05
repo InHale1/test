89d4dc3908adafb668ea1efe8984b272a2e70825
InHale1 <Luciferus-666@mail.ru>
2014-04-05 14:01:21 +0300

    Revert "Revert "first commit""
    This reverts commit c97c57e22ead00b6503d79fd1214baad1a83a315.

---
+++ idndex.php
@@ -0,0 +1,11 @@
+<div class="Search" style="float:left" >
+<form aсtion="Search.php" method = "POST">
+    Search:<br>
+    <select name="searchtype" size=1>
+    <option value="PID" selected>PID
+    </select> <br>
+    Что ищем?:<br> 
+    <input name="searchterm"> <br>
+    <input type=submit value="Поиск">
+</form>
+</div>
